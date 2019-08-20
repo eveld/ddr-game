190adfe5336432acbdad912565a972da297049d0
Erik Veld <eveld@hashicorp.com>
2019-08-20 15:23:49 +0200

    Implement joining and leaving games

--- JoinMenu.gd
+++ JoinMenu.gd
@@ -1,8 +1,6 @@
 extends Control
 
-var games_json = "[{\"id\": \"a\", \"started\": 3, \"away_id\": \"x\"}, {\"id\": \"b\", \"started\": 0, \"away_id\": \"\"}]"
 var selected_game_id = ""
-var games = JSON.parse(games_json).result
 
 func _ready():
 	var url = "http://localhost:9090/games"
@@ -12,11 +10,20 @@ func _ready():
 func _on_HTTP_get_games_request_completed(result, response_code, headers, body):
 	if(response_code == 200):
 		var response = JSON.parse(body.get_string_from_utf8()).result
+		selected_game_id = ""
 		for game in response:
 			if (game.started == 0 && game.away_id == ""):
 				selected_game_id = game.id
-				$join.disabled = false
 				break
+				
+		if selected_game_id != "":
+			$join.disabled = false
+			
+
+func _on_refresh_games_timeout():
+	var url = "http://localhost:9090/games"
+	var headers = ["Content-Type: application/json"]
+	$HTTP_get_games.request(url, headers, false, HTTPClient.METHOD_GET, "")
 
 func _on_cancel_pressed():
 	get_tree().change_scene("res://GameMenu.tscn")
@@ -27,7 +34,7 @@ func _on_cancel_pressed():
 func _on_join_pressed():
 	var url = "http://localhost:9090/games/" + selected_game_id + "/join"
 	var headers = ["Content-Type: application/json"]
-	$HTTP_create_game.request(url, headers, false, HTTPClient.METHOD_POST, "")
+	$HTTP_join_game.request(url, headers, false, HTTPClient.METHOD_POST, "")
 
 func _on_HTTP_join_game_request_completed(result, response_code, headers, body):
 	if(response_code == 200):
@@ -35,6 +42,3 @@ func _on_HTTP_join_game_request_completed(result, response_code, headers, body):
 		Game.set_game_id(response.id)
 		Game.set_player_id(response.away_id)
 		get_tree().change_scene("res://LobbyMenu.tscn")
-
-
-
--- LobbyMenu.gd
+++ LobbyMenu.gd
@@ -3,42 +3,55 @@ extends Control
 var ready_icon = preload("res://ready.png")
 var waiting_icon = preload("res://waiting.png")
 
-var players
-
 var p1_ready = false
 var p2_ready = false
 
+var all_ready = false
+
 func _ready():
-	players = Game.get_players()
-	if players == 1:
-		$p2.hide()
-		
-	if Game.is_master():
-		$p2.disabled = true
-	else:
-		$p1.disabled = true
+	$p1.disabled = !Game.is_master()
+	$p2.disabled = Game.is_master()
 
 func _process(delta):
-	if players == 1:
-		$start.disabled = !p1_ready
-	else:
-		$start.disabled = !(p1_ready && p2_ready)
+		$start.disabled = !all_ready
+		
+#
+# Poll game
+#
+func _on_refresh_game_timeout():
+	var url = "http://localhost:9090/games/" + Game.get_game_id()
+	var headers = ["Content-Type: application/json"]
+	$HTTP_get_game.request(url, headers, false, HTTPClient.METHOD_GET, "")
 
+func _on_HTTP_get_game_request_completed(result, response_code, headers, body):
+	if(response_code == 200):
+		var response = JSON.parse(body.get_string_from_utf8()).result
+		
+		all_ready = false
+		
+		p1_ready = response.home_ready
+		if p1_ready:
+			$p1.icon = ready_icon
+		else:
+			$p1.icon = waiting_icon
 
-func _on_Back_pressed():
-	get_tree().change_scene("res://GameMenu.tscn")
-
+		if response.away_id != "":
+			p2_ready = response.away_ready
+			if p2_ready:
+				$p2.icon = ready_icon
+			else:
+				$p2.icon = waiting_icon
+				
+			if p1_ready && p2_ready:
+				all_ready = true
+		else:
+			if p1_ready:
+				all_ready = true
 #
 # Ready player
 #
-func _on_P1_pressed():
-	_player_ready(Game.get_player_id())
-
-func _on_P2_pressed():
-	_player_ready(Game.get_player_id())
-
-func _player_ready(player):
-	var url = "http://localhost:9090/games/" + Game.get_game_id() + "/ready?player=" + player
+func _on_ready_pressed():
+	var url = "http://localhost:9090/games/" + Game.get_game_id() + "/ready?player=" + Game.get_player_id()
 	var headers = ["Content-Type: application/json"]
 	$HTTP_player_ready.request(url, headers, false, HTTPClient.METHOD_POST, "")
 
@@ -62,9 +75,6 @@ func _on_HTTP_player_ready_request_completed( result, response_code, headers, bo
 # Start the game
 #
 func _on_start_pressed():
-	_start_game()
-
-func _start_game():
 	var url = "http://localhost:9090/games/" + Game.get_game_id() + "/start"
 	var headers = ["Content-Type: application/json"]
 	$HTTP_start_game.request(url, headers, false, HTTPClient.METHOD_POST, "")
@@ -72,3 +82,13 @@ func _start_game():
 func _on_HTTP_start_game_request_completed(result, response_code, headers, body):
 	if(response_code == 200):
 		get_tree().change_scene("res://Main.tscn")
+
+
+func _on_back_pressed():
+	var url = "http://localhost:9090/games/" + Game.get_game_id() + "/leave?player=" + Game.get_player_id()
+	var headers = ["Content-Type: application/json"]
+	$HTTP_leave_game.request(url, headers, false, HTTPClient.METHOD_POST, "")
+
+func _on_HTTP_leave_game_request_completed(result, response_code, headers, body):
+	if(response_code == 200):
+		get_tree().change_scene("res://GameMenu.tscn")
