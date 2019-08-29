8065bd99ea620bb3acc9e71f7eebe5b9b3c1ef1e
Erik Veld <eveld@hashicorp.com>
2019-08-29 15:07:37 +0200

    Style menus

--- GameMenu.gd
+++ GameMenu.gd
@@ -1,45 +1,12 @@
 extends Control
 
-enum options {CREATE_GAME, JOIN_GAME}
-var active_option
-var create_button
-var join_button
-
-func _ready():
-	active_option = options.CREATE_GAME
-	
-	create_button = $background/center/vertical/horizontal/create
-	join_button = $background/center/vertical/horizontal/join
-	create_button.disabled = false
-	join_button.disabled = true
-
-func _input(event):
-	if event.is_action_pressed("e"):
-		if active_option == options.CREATE_GAME:
-			create_game()
-		else:
-			join_game()
-		
-	if event.is_action_pressed("q"):
-		cancel_game()
-		
-	if event.is_action_pressed("a"):
-		active_option = options.CREATE_GAME
-		create_button.disabled = false
-		join_button.disabled = true
-		
-	if event.is_action_pressed("d"):
-		active_option = options.JOIN_GAME
-		create_button.disabled = true
-		join_button.disabled = false
-
-func create_game():
+func _on_New_pressed():
 	Game.set_master()
 	get_tree().change_scene("res://CreateMenu.tscn")
-	
-func join_game():
+
+func _on_Join_pressed():
 	Game.reset_master()
 	get_tree().change_scene("res://JoinMenu.tscn")
 	
-func cancel_game():
+func _on_Back_pressed():
 	get_tree().change_scene("res://MainMenu.tscn")
