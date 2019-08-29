3f9a3165db4f53041bb478e5cf0c3878912f8fb2
Erik Veld <eveld@hashicorp.com>
2019-08-29 15:04:52 +0200

    style some menus

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,7 +1,17 @@
 extends Control
 
+func _input(event):
+	if event.is_action_pressed("e"):
+		start_game()
+		
+	if event.is_action_pressed("q"):
+		show_highscores()
+		
 func _on_Play_pressed():
+	start_game()
+	
+func start_game():
 	get_tree().change_scene("res://GameMenu.tscn")
-
-func _on_Option_pressed():
-	get_tree().change_scene("res://OptionMenu.tscn")
+	
+func show_highscores():
+	print("highscores")
