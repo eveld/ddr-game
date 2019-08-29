eedd0fc6fdb950c6361407eb8d8d6063b6d435f5
Erik Veld <eveld@hashicorp.com>
2019-08-29 15:35:42 +0200

    Add missing resources

--- GameMenu.gd
+++ GameMenu.gd
@@ -1,12 +1,45 @@
 extends Control
 
-func _on_New_pressed():
+enum options {CREATE_GAME, JOIN_GAME}
+var active_option
+var create_button
+var join_button
+
+func _ready():
+	active_option = options.CREATE_GAME
+	
+#	create_button = $background/center/vertical/horizontal/create
+#	join_button = $background/center/vertical/horizontal/join
+#	create_button.disabled = false
+#	join_button.disabled = true
+
+func _input(event):
+	if event.is_action_pressed("e"):
+		if active_option == options.CREATE_GAME:
+			create_game()
+		else:
+			join_game()
+		
+	if event.is_action_pressed("q"):
+		cancel_game()
+		
+	if event.is_action_pressed("a"):
+		active_option = options.CREATE_GAME
+#		create_button.disabled = false
+#		join_button.disabled = true
+		
+	if event.is_action_pressed("d"):
+		active_option = options.JOIN_GAME
+#		create_button.disabled = true
+#		join_button.disabled = false
+
+func create_game():
 	Game.set_master()
 	get_tree().change_scene("res://CreateMenu.tscn")
-
-func _on_Join_pressed():
+	
+func join_game():
 	Game.reset_master()
 	get_tree().change_scene("res://JoinMenu.tscn")
 	
-func _on_Back_pressed():
+func cancel_game():
 	get_tree().change_scene("res://MainMenu.tscn")
